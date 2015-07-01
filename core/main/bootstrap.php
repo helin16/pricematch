f5ece8598bcfb44014a4e9178f85bf113b012bef
lhe <lhe@bytecraft.com.au>
2015-07-01 11:54:26 +1000

    initial

---
+++ core/main/bootstrap.php
@@ -0,0 +1,41 @@
+<?php
+date_default_timezone_set('UTC');
+/**
+ * Boostrapper for the Core module
+ *
+ * @package Core
+ * @author  lhe
+ */
+abstract class SystemCoreAbstract
+{
+    /**
+     * autoloading function
+     *
+     * @param string $className The class that we are trying to autoloading
+     *
+     * @return boolean Whether we loaded the class
+     */
+	public static function autoload($className)
+	{
+		$base = dirname(__FILE__);
+		$autoloadPaths = array(
+			$base . '/conf/',
+			$base . '/db/',
+			$base . '/entity/',
+			$base . '/entity/system/',
+			$base . '/entity/tools/'
+		);
+		foreach ($autoloadPaths as $path)
+		{
+			if (file_exists($file = trim($path . $className . '.php')))
+			{
+				require_once $file;
+				return true;
+			}
+		}
+		return false;
+	}
+}
+spl_autoload_register(array('SystemCoreAbstract','autoload'));
+
+?>
